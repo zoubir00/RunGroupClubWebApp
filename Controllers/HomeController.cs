147b0829785ef8ed439558f4576a335b7770f50e
zoubir2000 <benzoubir2000@gmail.com>
2022-12-31 11:08:35 +0100

    create Ip Info to know where is user and show him all the clubs near of him

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroupClubWebApp.Helpers;
+using RunGroupClubWebApp.Interfaces;
 using RunGroupClubWebApp.Models;
+using RunGroupClubWebApp.ViewModel;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace RunGroupClubWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeVm = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io/105.159.125.60?token=1f136264abd3ab";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo MyRJI = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = MyRJI.EnglishName;
+                homeVm.City = ipInfo.City;
+                homeVm.State = ipInfo.Region;
+                if (homeVm.City != null)
+                {
+                    homeVm.clubs = await _clubRepository.GetClubsByCity(homeVm.City);
+                }
+                else
+                {
+                    homeVm.clubs = null;
+                }
+                return View(homeVm);
+
+            }catch(Exception ex)
+            {
+                homeVm.clubs = null;
+            }
+ 
+            return View(homeVm);
         }
 
         public IActionResult Privacy()
---
+++ ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RunGroupClubWebApp.Models;
+
+namespace RunGroupClubWebApp.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
